8c89cc54e44c05e55b88362f1e27abbce989c457
KanishkVijaywargiya <secretcodersociety@gmail.com>
2024-08-23 00:10:51 +0530

    password regex bug resolved

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,4 @@ export const DB_NAME = "youtubeproject";
 // regex expressions
 export const EMAIL_REGEX = /^[\w.-]+@[\w.-]+\.\w{2,4}$/;
 export const PASSWORD_REGEX =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
